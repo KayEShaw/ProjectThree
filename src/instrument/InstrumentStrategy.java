5e3c942a5590575c5f197ed701b6d5d1c4bf533e
Amwaj <amwajsabri@gmail.com>
2025-04-04 18:34:23 -0500

    added javadoc for InstrumentStrategy class

--- src/instrument/InstrumentStrategy.java
+++ src/instrument/InstrumentStrategy.java
@@ -3,6 +3,12 @@ package instrument;
 import javax.sound.midi.Track;
 
 public interface InstrumentStrategy {
+	/**
+	 * This method
+	 * @param track; the music we're adding to the MIDI event
+	 * @param channel; where the instrument is assigned, each
+	 * 					instrument is assigned to a spot
+	 */
 	void applyInstrument(Track track, int channel); 
 }
  
