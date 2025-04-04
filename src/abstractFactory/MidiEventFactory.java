66791aa926a96710a921fef2f26068e1ab421d5d
Kayln Shaw <iam>
2025-04-04 16:54:13 -0500

    adding java docs to some methods

--- src/abstractFactory/MidiEventFactory.java
+++ src/abstractFactory/MidiEventFactory.java
@@ -4,6 +4,24 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 
 public interface MidiEventFactory {
+	/**
+	 * creates an on note with the assigned variables
+	 * @param tick
+	 * @param note
+	 * @param velocity
+	 * @param channel
+	 * @return MidiEvent
+	 * @throws InvalidMidiDataException
+	 */
 MidiEvent CreateNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
+	/**
+	 * creates an off note with the assigned variables
+	 * @param tick
+	 * @param note
+	 * @param velocity
+	 * @param channel
+	 * @return
+	 * @throws InvalidMidiDataException
+	 */
 MidiEvent CreateNoteOff(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
 }
