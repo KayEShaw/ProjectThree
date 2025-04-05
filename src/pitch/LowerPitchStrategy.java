47a859bda51daf2b55a6b432d2f6507b068e717b
Amwaj <amwajsabri@gmail.com>
2025-04-04 19:31:14 -0500

    added javadoc to LowerPitchStrategy

--- src/pitch/LowerPitchStrategy.java
+++ src/pitch/LowerPitchStrategy.java
@@ -2,6 +2,11 @@ package pitch;
 
 public class LowerPitchStrategy implements PitchStrategy{
 	
+	/**
+	 * This method overrides the instance created in 
+	 * PitchStrategy and uses it here to decrease
+	 * the pitch by 2 semitones
+	 */
 	@Override
 	public int modifyPitch(int note) {
 		return note -= 2; 
