db8a7e509923f2a3b5ab24064ee2e39e2f3ff904
Amwaj <amwajsabri@gmail.com>
2025-04-04 19:28:57 -0500

    added javadoc for HigherPitchStrategy class

--- src/pitch/HigherPitchStrategy.java
+++ src/pitch/HigherPitchStrategy.java
@@ -2,6 +2,11 @@ package pitch;
 
 public class HigherPitchStrategy implements PitchStrategy{
 	
+	/**
+	 * This method overrides the instance created in 
+	 * PitchStrategy and uses it here to increase
+	 * the pitch by 2 semitones
+	 */
 	@Override
 	public int modifyPitch(int note) {
 		return note += 2; 
