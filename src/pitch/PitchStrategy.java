228319d9ba31c2e8e388940b47b5ed3064f58476
Amwaj <amwajsabri@gmail.com>
2025-04-04 19:31:55 -0500

    added javadoc for PitchStrategy interface

--- src/pitch/PitchStrategy.java
+++ src/pitch/PitchStrategy.java
@@ -1,5 +1,10 @@
 package pitch;
 
 public interface PitchStrategy {
+	/**
+	 * This interface declares a method to be used in implementing classes
+	 * @param note; helps implementing classes to change the pitch of the music
+	 * 				by increasing or decreasing the note
+	 */
 	int modifyPitch(int note);
 }
